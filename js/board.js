ec32d9985d16cc99cf84bf346c2b0e290beb8489
Kenneth Chen <kennethchendbgt@gmail.com>
2017-01-19 14:11:13 -0500

    Set up grid for Board

--- js/board.js
+++ js/board.js
@@ -1,6 +1,19 @@
 class Board {
   constructor() {
+    this.grid = Board.makeGrid();
+  }
 
+  static makeGrid() {
+    const grid = [];
+
+    for (let rowIdx = 0; rowIdx < 4; rowIdx++) {
+      grid.push([]);
+      for (let colIdx = 0; colIdx < 4; colIdx++) {
+        grid[rowIdx].push(null);
+      }
+    }
+
+    return grid;
   }
 }
 
