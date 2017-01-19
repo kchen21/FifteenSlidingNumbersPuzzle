a1a3f65478b1d3c37415184e584b27d79166784f
Kenneth Chen <kennethchendbgt@gmail.com>
2017-01-19 15:31:16 -0500

    Create playMove method for Game

--- js/game.js
+++ js/game.js
@@ -10,15 +10,20 @@ class Game {
   playMove(currentPos) {
     const rowIdx = currentPos[0];
     const colIdx = currentPos[1];
+    const selectedEl = this.board.getElement(currentPos);
 
     if (this.board.getElement([rowIdx - 1, colIdx]) === "space") {
-
+      this.board.setElement([rowIdx - 1, colIdx], selectedEl);
+      this.board.setElement(currentPos, "space");
     } else if (this.board.getElement([rowIdx + 1, colIdx]) === "space") {
-
+      this.board.setElement([rowIdx + 1, colIdx], selectedEl);
+      this.board.setElement(currentPos, "space");
     } else if (this.board.getElement([rowIdx, colIdx - 1]) === "space") {
-
+      this.board.setElement([rowIdx, colIdx - 1], selectedEl);
+      this.board.setElement(currentPos, "space");
     } else if (this.board.getElement([rowIdx, colIdx + 1]) === "space") {
-      
+      this.board.setElement([rowIdx, colIdx + 1], selectedEl);
+      this.board.setElement(currentPos, "space");
     }
   }
 
