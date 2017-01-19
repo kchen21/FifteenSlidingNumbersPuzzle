7112d2cb05c95662758b5ba0450c66b31b8bf29c
Kenneth Chen <kennethchendbgt@gmail.com>
2017-01-19 14:53:03 -0500

    Create populateGrid method for Board

--- js/board.js
+++ js/board.js
@@ -3,6 +3,22 @@ class Board {
     this.grid = Board.makeGrid();
   }
 
+  populateGrid() {
+    const gridElements = ["space"];
+
+    for (let i = 1; i <= 15; i++) {
+      gridElements.push(i.toString());
+    }
+
+    Board.shuffle(gridElements);
+
+    for (let rowIdx = 0; rowIdx < 4; rowIdx++) {
+      for (let colIdx = 0; colIdx < 4; colIdx++) {
+        this.grid[rowIdx][colIdx] = gridElements.pop();
+      }
+    }
+  }
+
   static makeGrid() {
     const grid = [];
 
@@ -14,6 +30,13 @@ class Board {
     }
 
     return grid;
+  }
+
+  static shuffle(arr) {
+    for (let i = arr.length; i > 0; i--) {
+      let j = Math.floor(Math.random() * i);
+      [arr[i - 1], arr[j]] = [arr[j], arr[i -1]];
+    }
   }
 }
 
